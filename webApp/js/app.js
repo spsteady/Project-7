29b017c4310eb7345f2891ba33f1acbfd7384c3d
Steven Steady <spsteady@github.com>
2021-05-19 14:15:21 -0500

    line chart update

--- webApp/js/app.js
+++ webApp/js/app.js
@@ -49,51 +49,49 @@ timeSelect.forEach((selection) => {
   });
 });
 
-function trafficLine() {
-  let hourData = {
-    labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
-    datasets: [{
-        data: [187, 475, 250, 500, 375, 450, 312, 470, 566, 375, 625, 200],
-        backgroundColor: "rgba(116, 119, 191,.3)",
-        borderColor: "#7477BF",
-        fill: true,
-        borderWidth: 1,
-    }]
-  };
-  
-  let dayData = {
-    labels: ["1-73", "74-146", "147-220", "221-294", "295-365"],
-    datasets: [{
-        data: [625, 500, 1000, 678, 1250],
-        backgroundColor: "rgba(116, 119, 191,.3)",
-        borderColor: "#7477BF",
-        fill: true,
-        borderWidth: 1,
-    }]
-  };
+let hourData = {
+  labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
+  datasets: [{
+      data: [187, 475, 250, 500, 375, 450, 312, 470, 566, 375, 625, 200],
+      backgroundColor: "rgba(116, 119, 191,.3)",
+      borderColor: "#7477BF",
+      fill: true,
+      borderWidth: 1,
+  }]
+};
 
-  let weekData = {
-    labels: ["1-7", "8-14", "15-21", "22-28", "29-35", "36-42", "42-48", "49-52"],
-    datasets: [{
-        data: [750, 1250, 1000, 2000, 1500, 1750, 2250, 3200],
-        backgroundColor: "rgba(116, 119, 191,.3)",
-        borderColor: "#7477BF",
-        fill: true,
-        borderWidth: 1,
-    }]
-  };
+let dayData = {
+  labels: ["1-73", "74-146", "147-220", "221-294", "295-365"],
+  datasets: [{
+      data: [625, 500, 1000, 678, 1250],
+      backgroundColor: "rgba(116, 119, 191,.3)",
+      borderColor: "#7477BF",
+      fill: true,
+      borderWidth: 1,
+  }]
+};
 
-  let monthData = {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-    datasets: [{
-        data: [2700, 3450, 4000, 2000, 1500, 1750, 5000, 1850, 2250, 1500, 2500, 4120],
-        backgroundColor: "rgba(116, 119, 191,.3)",
-        borderColor: "#7477BF",
-        fill: true,
-        borderWidth: 1,
-    }]
-  };
-}
+let weekData = {
+  labels: ["1-7", "8-14", "15-21", "22-28", "29-35", "36-42", "42-48", "49-52"],
+  datasets: [{
+      data: [750, 1250, 1000, 2000, 1500, 1750, 2250, 3200],
+      backgroundColor: "rgba(116, 119, 191,.3)",
+      borderColor: "#7477BF",
+      fill: true,
+      borderWidth: 1,
+  }]
+};
+
+let monthData = {
+  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+  datasets: [{
+      data: [2700, 3450, 4000, 2000, 1500, 1750, 5000, 1850, 2250, 1500, 2500, 4120],
+      backgroundColor: "rgba(116, 119, 191,.3)",
+      borderColor: "#7477BF",
+      fill: true,
+      borderWidth: 1,
+  }]
+};
 
 let trafficOptions = {
   aspectRatio: 2.5,
@@ -114,14 +112,12 @@ let trafficOptions = {
 
 let trafficChart = new Chart(trafficCanvas, {
   type: 'line',
-  data: trafficLine(),
+  data: monthData,
   options: trafficOptions
 });
 //#endregion
 
 //#region DAILY CHART (BAR GRAPH)
-
-
 const dailyData = {
   labels: ["S", "M", "T", "W", "T", "F", "S"],
   datasets: [{
@@ -153,8 +149,6 @@ let dailyChart = new Chart(dailyCanvas, {
 //#endregion
 
 //#region DOUGHNUT CHART
-
-
 const mobileData = {
   labels: ["Desktop", "Tablet", "Phones"],
   datasets: [{
@@ -189,8 +183,6 @@ let mobileChart = new Chart(mobileCanvas, {
 //#endregion
 
 //#region MESSAGE USERS SECTION
-
-
 send.addEventListener('click', () => {
   if (user.value === "" && message.value === "") {
     alert("Please fill out user and message fields before sending");
